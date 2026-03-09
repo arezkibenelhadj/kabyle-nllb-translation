6d7d9e851516d53786d7368f5da9331773733266
arezkibenelhadj <arezki.benelhadj@ummto.dz>
2026-03-09 07:32:04 +0100

    Correction Seq2SeqTrainer : suppression du tokenizer, compatible Transformers latest

--- scripts/train.py
+++ scripts/train.py
@@ -145,7 +145,6 @@ if __name__ == "__main__":
         args=training_args,
         train_dataset=train_ds,
         eval_dataset=dev_ds,
-        tokenizer=tokenizer,
         compute_metrics=compute_metrics,
     )
 
