549ddb8045758f8f4a2ca7d803d098d802387d7f
arezkibenelhadj <arezki.benelhadj@ummto.dz>
2026-03-10 06:16:04 +0100

    Réduction de la taille du batch a 1

--- scripts/train.py
+++ scripts/train.py
@@ -120,8 +120,8 @@ if __name__ == "__main__":
 
         learning_rate=3e-5,
 
-        per_device_train_batch_size=2,
-        per_device_eval_batch_size=2,
+        per_device_train_batch_size=1,
+        per_device_eval_batch_size=1,
 
         gradient_accumulation_steps=4,
 
