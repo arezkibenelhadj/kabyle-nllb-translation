8538f34f8f4cbacd7265482e7e4eb45f78166577
arezkibenelhadj <arezki.benelhadj@ummto.dz>
2026-03-25 09:56:59 +0100

    adapt for google colab

--- scripts/train.py
+++ scripts/train.py
@@ -90,15 +90,20 @@ def preprocess(example):
     inputs = example[input_col]
     targets = example[target_col]
 
-    model_inputs = tokenizer(inputs, max_length=128, truncation=True)
+    model_inputs = tokenizer(
+        inputs,
+        max_length=128,
+        truncation=True
+    )
 
-    with tokenizer.as_target_tokenizer():
-        labels = tokenizer(targets, max_length=128, truncation=True)
+    labels = tokenizer(
+        targets,
+        max_length=128,
+        truncation=True
+    )
 
     model_inputs["labels"] = labels["input_ids"]
     return model_inputs
-
-dataset = dataset.map(preprocess, batched=True)
 
 # ========================
 # COLLATOR
@@ -127,11 +132,11 @@ def compute_metrics(eval_preds):
 # ========================
 training_args = Seq2SeqTrainingArguments(
     output_dir=save_path,  # ✅ checkpoints sur Drive
-    per_device_train_batch_size=8,
-    per_device_eval_batch_size=8,
-    gradient_accumulation_steps=2,
+    per_device_train_batch_size=2,
+    per_device_eval_batch_size=2,
+    gradient_accumulation_steps=1,
     learning_rate=2e-4,
-    num_train_epochs=3,
+    num_train_epochs=1,
     logging_dir=os.path.join(save_path, "logs"),
     logging_steps=100,
     evaluation_strategy="epoch",
